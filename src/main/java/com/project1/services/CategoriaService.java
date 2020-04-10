b95f9b5f689b8359ac53ceae7b304eaffab54d79
Douglas Santanna <dpire.santanna@gmail.com>
2020-04-10 10:34:13 -0300

    Excluindo uma Categoria com DELETE

--- src/main/java/com/project1/services/CategoriaService.java
+++ src/main/java/com/project1/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.project1.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.project1.domain.Categoria;
 import com.project1.repositories.CategoriaRepository;
+import com.project1.services.exception.DataIntegrityException;
 import com.project1.services.exception.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria objetoCategoria) {
 		buscar(objetoCategoria.getId());
 		return repositorioCategoria.save(objetoCategoria);
+	}
+	
+	public void delete(Integer id) {
+		buscar(id);
+		try {
+			repositorioCategoria.deleteById(id); 
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos associados");
+		}
 	}
 }
